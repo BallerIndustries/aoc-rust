30338d18d551d8e4e7e85b2a4e44d0b6b79b589b
Angus Cheng <baller.industries@hotmail.com>
2021-11-24 20:17:25 +0800

    initial commit, part a of 2015 with some tests

---
+++ src/main.rs
@@ -0,0 +1,6 @@
+mod puzzle1;
+
+fn main() {
+    println!("Good morning Cruise");
+}
+
---
+++ src/puzzle1.rs
@@ -0,0 +1,40 @@
+use std::fs;
+
+pub fn part_a(text: String) -> i64 {
+    let mut counter: i64 = 0;
+
+    for (_, c) in text.chars().enumerate() {
+        if c == '(' {
+            counter += 1
+        }
+        else if c == ')' {
+            counter -= 1
+        }
+    }
+
+    return counter
+}
+
+pub fn read_all_text(filename: &str) -> String {
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+    return contents
+}
+
+
+#[cfg(test)]
+mod tests {
+    use crate::puzzle1::*;
+
+    #[test]
+    fn puzzle_part_a() {
+        let text = read_all_text("./resources/2016/1.txt");
+        assert_eq!(part_a(text), 138)
+    }
+
+    #[test]
+    fn example_part_a() {
+        let text = String::from("(())");
+        assert_eq!(part_a(text), 0)
+    }
+}
