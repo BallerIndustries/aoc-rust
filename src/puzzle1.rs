c3e40c98a899dbe8303ecab5f482788c163ad669
Angus Cheng <baller.industries@hotmail.com>
2021-12-01 13:45:04 +0800

    doing it functionally

--- src/puzzle1.rs
+++ src/puzzle1.rs
@@ -1,49 +1,25 @@
+use std::ops::Range;
+
 pub fn part_a(text: String) -> i32 {
-    let mut values: Vec<i32> = Vec::new();
+    let lines: Vec<&str> = text.lines().collect();
 
-    for line in text.lines() {
-        values.push(line.parse::<i32>().unwrap());
-    }
-
-    let mut count = 0;
-
-    for index in 1..values.len() {
-        let prev = values[index-1];
-        let current = values[index];
-
-        if (current > prev) {
-            count += 1
-        }
-    }
-
-    return count;
+    return (1..lines.len()).filter(|index| {
+        let prev = lines[*index-1].parse::<i32>().unwrap();
+        let current = lines[*index].parse::<i32>().unwrap();
+        current > prev
+    }).count() as i32;
 }
 
 pub fn part_b(text: String) -> i32 {
-    let mut values: Vec<i32> = Vec::new();
+    let values: Vec<i32> = text.lines().map(|x| {
+        x.parse::<i32>().unwrap()
+    }).collect();
 
-    for line in text.lines() {
-        values.push(line.parse::<i32>().unwrap());
-    }
-
-    let mut jurValues: Vec<i32> = Vec::new();
-
-    for index in 0..(values.len() - 2) {
-        jurValues.push(values[index] + values[index+1] + values[index+2])
-    }
-
-    let mut count = 0;
-
-    for index in 1..jurValues.len() {
-        let prev = jurValues[index-1];
-        let current = jurValues[index];
-
-        if (current > prev) {
-            count += 1
-        }
-    }
-
-    return count;
+    return (1..values.len()-2).filter(|index| {
+        let prev: i32 = (*index-1..*index+2).map(|i| values[i]).sum();
+        let current: i32 = (*index..*index+3).map(|i| values[i]).sum();
+        current > prev
+    }).count() as i32;
 }
 
 #[cfg(test)]
@@ -55,17 +31,12 @@ mod tests {
     #[test]
     fn puzzle_part_a() {
         let text = read_all_text(FILENAME);
-        assert_eq!(part_a(text), 0)
+        assert_eq!(part_a(text), 1154)
     }
 
     #[test]
     fn puzzle_part_b() {
         let text = read_all_text(FILENAME);
-        assert_eq!(part_b(text), 0)
-    }
-
-    #[test]
-    fn example_part_a() {
-        assert_eq!(part_a("".into()), 0);
+        assert_eq!(part_b(text), 1127)
     }
 }
