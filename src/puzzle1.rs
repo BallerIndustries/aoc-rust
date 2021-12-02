33d5c5e2bf14e06e0e676a9f6506d140862a8f08
Angus Cheng <baller.industries@hotmail.com>
2021-12-02 13:35:51 +0800

    removing warnings

--- src/puzzle1.rs
+++ src/puzzle1.rs
@@ -1,5 +1,3 @@
-use std::ops::Range;
-
 pub fn part_a(text: String) -> i32 {
     let lines: Vec<&str> = text.lines().collect();
 
--- src/puzzle2.rs
+++ src/puzzle2.rs
@@ -1,5 +1,3 @@
-use std::cmp::{max, min};
-
 pub fn part_a(text: String) -> i32 {
     let mut x = 0;
     let mut y = 0;
@@ -13,13 +11,12 @@ pub fn part_a(text: String) -> i32 {
         }
         else if tmp[0] == "up" {
             y -= value;
-            y = max(y, 0)
         }
         else if tmp[0] == "down" {
             y += value
         }
 
-        println!("{},{}", x, y)
+        // println!("{},{}", x, y)
     }
 
     return x * y;
@@ -36,7 +33,7 @@ pub fn part_b(text: String) -> i32 {
 
         if tmp[0] == "forward" {
             x += value;
-            y += (aim * value);
+            y += aim * value;
         }
         else if tmp[0] == "up" {
             aim -= value
@@ -45,8 +42,8 @@ pub fn part_b(text: String) -> i32 {
             aim += value
         }
 
-        println!("{}", line);
-        println!("{},{} aim = {}\n", x, y, aim);
+        // println!("{}", line);
+        // println!("{},{} aim = {}\n", x, y, aim);
     }
 
     return x * y;
@@ -61,39 +58,34 @@ mod tests {
     #[test]
     fn puzzle_part_a() {
         let text = read_all_text(FILENAME);
-        assert_eq!(part_a(text), 0)
+        assert_eq!(part_a(text), 2322630)
     }
 
     #[test]
     fn puzzle_part_b() {
         let text = read_all_text(FILENAME);
-        assert_eq!(part_b(text), 0)
+        assert_eq!(part_b(text), 2105273490)
     }
 
     #[test]
     fn example_part_a() {
-        assert_eq!(part_a("forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2".into()), 0);
+        let x = "forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2";
+        assert_eq!(part_a(x.into()), 150);
     }
 
-
-
-        #[test]
+    #[test]
     fn example_part_b() {
-        assert_eq!(part_b("forward 5
-down 5
-forward 8
-up 3
-down 8
-forward 2".into()), 900);
+        let x = "forward 5
+            down 5
+            forward 8
+            up 3
+            down 8
+            forward 2";
+        assert_eq!(part_b(x.into()), 900);
     }
-
-
-
-
-
 }
