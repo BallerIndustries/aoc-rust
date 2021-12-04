6e74cffc70b8ea9dc1ea7e797f6d018e79d30714
Angus Cheng <baller.industries@hotmail.com>
2021-12-04 18:26:17 +0800

    refactoring, maybe this will teach me how this fucking borrow checker works

--- src/puzzle4.rs
+++ src/puzzle4.rs
@@ -1,6 +1,33 @@
 use std::collections::HashSet;
 
 pub fn part_a(text: String) -> i32 {
+    let (numbers, mut boards) = parse_text(text);
+
+    for number in numbers {
+        for board in boards.iter_mut() {
+            zero_out_match(number, board);
+
+            if has_bingo(board) {
+                let unmarked_sum: i32 = board.iter().flatten().sum();
+                return unmarked_sum * number;
+            }
+        }
+    }
+
+    panic!("Ruh oh")
+}
+
+fn zero_out_match(number: i32, board: &mut Vec<Vec<i32>>) {
+    for row in board.iter_mut() {
+        for value in row.iter_mut() {
+            if *value == number {
+                *value = 0
+            }
+        }
+    }
+}
+
+fn parse_text(text: String) -> (Vec<i32>, Vec<Vec<Vec<i32>>>) {
     let lines: Vec<&str> = text.lines().collect();
     let numbers: Vec<i32> = lines.get(0).unwrap().split(",").map(|x| x.parse::<i32>().unwrap()).collect();
 
@@ -12,34 +39,12 @@ pub fn part_a(text: String) -> i32 {
         let board_lines: Vec<&str> = board_text.lines().collect();
 
         let board: Vec<Vec<i32>> = board_lines.iter().map(|l| {
-           l.split_whitespace().map(|t| t.parse::<i32>().unwrap()).collect()
+            l.split_whitespace().map(|t| t.parse::<i32>().unwrap()).collect()
         }).collect();
 
         boards.push(board);
     }
-
-    for number in numbers {
-        // Zero out any matches
-        for board in boards.iter_mut() {
-            for row in board.iter_mut() {
-                for value in row.iter_mut() {
-                    if *value == number {
-                        *value = 0
-                    }
-                }
-            }
-        }
-
-        for board in &boards {
-            if has_bingo(board) {
-                println!("That's a bingo!");
-                let unmarked_sum: i32 = board.iter().flatten().sum();
-                return unmarked_sum * number;
-            }
-        }
-    }
-
-    panic!("Not implemented")
+    (numbers, boards)
 }
 
 fn has_bingo(board: &Vec<Vec<i32>>) -> bool {
@@ -64,25 +69,8 @@ fn has_bingo(board: &Vec<Vec<i32>>) -> bool {
 }
 
 pub fn part_b(text: String) -> i32 {
-    let lines: Vec<&str> = text.lines().collect();
-    let numbers: Vec<i32> = lines.get(0).unwrap().split(",").map(|x| x.parse::<i32>().unwrap()).collect();
-
-    let pieces: Vec<&str> = text.split("\n\n").collect();
-    let mut boards: Vec<Vec<Vec<i32>>> = Vec::new();
-
-    for index in 1..pieces.len() {
-        let board_text = pieces[index];
-        let board_lines: Vec<&str> = board_text.lines().collect();
-
-        let board: Vec<Vec<i32>> = board_lines.iter().map(|l| {
-            l.split_whitespace().map(|t| t.parse::<i32>().unwrap()).collect()
-        }).collect();
-
-        boards.push(board);
-    }
-
+    let (numbers, mut boards) = parse_text(text);
     let board_len = boards.len();
-
     let mut board_indexes: HashSet<usize> = HashSet::new();
 
     for number in numbers {
@@ -92,13 +80,7 @@ pub fn part_b(text: String) -> i32 {
                 continue
             }
 
-            for row in board.iter_mut() {
-                for value in row.iter_mut() {
-                    if *value == number {
-                        *value = 0
-                    }
-                }
-            }
+            zero_out_match(number, board);
 
             if has_bingo(board) {
                 board_indexes.insert(board_index);
@@ -111,7 +93,7 @@ pub fn part_b(text: String) -> i32 {
         }
     }
 
-    panic!("Not implemented")
+    panic!("Ruh oh")
 }
 
 #[cfg(test)]
