ed21c7a7cbb2666667535536e294056ab1026e4a
Angus Cheng <baller.industries@hotmail.com>
2021-12-13 17:08:28 +0800

    clean up

--- src/puzzle13.rs
+++ src/puzzle13.rs
@@ -40,12 +40,10 @@ pub fn part_a(text: String) -> i32 {
 }
 
 fn parse_points(temp: &Vec<&str>) -> Vec<Point> {
-    let point_text: &str = temp[0];
-    let points: Vec<Point> = point_text.lines().map(|line| {
+    temp[0].lines().map(|line| {
         let jur: Vec<i32> = line.split(",").map(|it| it.parse::<i32>().unwrap()).collect();
         return Point { x: jur[0], y: jur[1] }
-    }).collect();
-    points
+    }).collect()
 }
 
 pub fn part_b(text: String) -> i32 {
@@ -107,6 +105,27 @@ mod tests {
     use super::*;
     use crate::util::*;
     const FILENAME: &str = "./resources/2021/13.txt";
+    const EXAMPLE_A: &str = "6,10
+0,14
+9,10
+0,3
+10,4
+4,11
+6,0
+6,12
+4,1
+0,13
+10,12
+3,4
+3,0
+8,4
+1,10
+2,14
+8,10
+9,0
+
+fold along y=7
+fold along x=5";
 
     #[test]
     fn puzzle_part_a() {
@@ -122,51 +141,11 @@ mod tests {
 
     #[test]
     fn example_part_a() {
-        assert_eq!(part_a("6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
-
-fold along y=7
-fold along x=5".into()), 17);
+        assert_eq!(part_a(EXAMPLE_A.into()), 17);
     }
 
     #[test]
     fn example_part_b() {
-        assert_eq!(part_b("6,10
-0,14
-9,10
-0,3
-10,4
-4,11
-6,0
-6,12
-4,1
-0,13
-10,12
-3,4
-3,0
-8,4
-1,10
-2,14
-8,10
-9,0
-
-fold along y=7
-fold along x=5".into()), 16);
+        assert_eq!(part_b(EXAMPLE_A.into()), 16);
     }
 }
