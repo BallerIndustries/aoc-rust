fc1581259c03f4039de33fe2afa7e93af5f3c236
Angus Cheng <baller.industries@hotmail.com>
2021-12-14 22:02:53 +0800

    WIP

--- src/puzzle14.rs
+++ src/puzzle14.rs
@@ -1,72 +1,14 @@
-use std::collections::{HashMap, LinkedList};
-use crate::puzzle14::address::Nil;
-
-#[derive(Clone)]
-enum address {
-    address(Box<Node>),
-    Nil,
-}
-
-#[derive(Clone)]
-struct Node {
-    value: char,
-    next: address,
-}
-
-impl Node {
-    fn append(&mut self, elem: char) {
-        match self.next {
-            address::address(ref mut next_address) => {
-                next_address.append(elem);
-            }
-            address::Nil => {
-                let node = Node {
-                    value: elem,
-                    next: address::Nil,
-                };
-                self.next = address::address(Box::new(node))
-            }
-        }
-    }
-
-    fn insert_after(&mut self, elem: char) {
-        match self.next {
-            address::address(ref mut next_address) => {
-
-                let node = Node {
-                    value: elem,
-                    next: self.next.clone(),
-                };
-                self.next = address::address(Box::new(node))
-
-
-            }
-            address::Nil => {
-                let node = Node {
-                    value: elem,
-                    next: address::Nil,
-                };
-                self.next = address::address(Box::new(node))
-            }
-        }
-    }
-}
+use std::collections::{HashMap};
 
 pub fn part_a(text: String) -> i64 {
     let pieces: Vec<&str> = text.split("\n\n").collect();
     let mut rules: HashMap<(char, char), char> = HashMap::new();
-    let mut char_to_count: HashMap<char, i64> = HashMap::new();
-
 
     for rule in pieces[1].lines() {
         let tmp: Vec<&str> = rule.split(" -> ").collect();
         let from_chars: Vec<char> = tmp[0].chars().collect();
         let to_chars: Vec<char> = tmp[1].chars().collect();
         rules.insert((from_chars[0], from_chars[1]), to_chars[0]);
-
-        char_to_count.insert(from_chars[0], 0);
-        char_to_count.insert(from_chars[1], 0);
-        char_to_count.insert(to_chars[0], 0);
     }
 
     let mut counter = 0;
@@ -75,15 +17,14 @@ pub fn part_a(text: String) -> i64 {
     while counter < 10 {
         compound = run_step(&rules, &compound);
         counter += 1;
-
-        let delta = count_delta(&mut char_to_count, &mut compound);
-        println!("delta = {}", delta);
     }
 
-    return count_delta(&mut char_to_count, &mut compound);
+    return count_delta(&compound);
 }
 
-fn count_delta(char_to_count: &mut HashMap<char, i64>, compound: &mut String) -> i64 {
+fn count_delta(compound: &str) -> i64 {
+    let mut char_to_count: HashMap<char, i64> = HashMap::new();
+
     for char in compound.chars() {
         let counter = char_to_count.entry(char).or_insert(0);
         *counter += 1;
@@ -91,7 +32,6 @@ fn count_delta(char_to_count: &mut HashMap<char, i64>, compound: &mut String) ->
 
     let max = char_to_count.values().max().unwrap();
     let min = char_to_count.values().min().unwrap();
-
     return max - min;
 }
 
@@ -111,103 +51,114 @@ fn run_step(rules: &HashMap<(char, char), char>, compound: &str) -> String {
     return buffer;
 }
 
-fn run_step_v2(rules: &HashMap<(char, char), char>, head: &mut Node) -> String {
-    let mut current = head;
-
-    while matches!(current.next, Nil) == false {
-        let a = current.value;
-
-        let b = match current.next {
-            address::address(ref newAddress) => {
-                newAddress.value
-            }
-            address::Nil => {
-                panic!("aaa")
-            }
-        };
-
-        let middler = rules.get(&(a, b)).unwrap();
-        current.insert_after(*middler);
-        println!("ok")
-    }
-
-    panic!("aaa");
-    return "".into()
-
-
-    // let chars: Vec<char> = compound.chars().collect();
-    // let mut buffer: String = "".into();
-    // buffer.push(chars[0]);
-    //
-    // for index in 0..chars.len()-1 {
-    //     let tuple = (chars[index], chars[index+1]);
-    //     let in_between_char = rules.get(&tuple).unwrap();
-    //
-    //     buffer.push(*in_between_char);
-    //     buffer.push(tuple.1);
-    // }
-    //
-    // return buffer;
-}
-
 pub fn part_b(text: String) -> i64 {
     let pieces: Vec<&str> = text.split("\n\n").collect();
     let mut rules: HashMap<(char, char), char> = HashMap::new();
-    let mut char_to_count: HashMap<char, i64> = HashMap::new();
+    let mut compound: String = pieces[0].into();
 
     for rule in pieces[1].lines() {
         let tmp: Vec<&str> = rule.split(" -> ").collect();
         let from_chars: Vec<char> = tmp[0].chars().collect();
         let to_chars: Vec<char> = tmp[1].chars().collect();
         rules.insert((from_chars[0], from_chars[1]), to_chars[0]);
-
-        char_to_count.insert(from_chars[0], 0);
-        char_to_count.insert(from_chars[1], 0);
-        char_to_count.insert(to_chars[0], 0);
     }
 
-    let mut counter = 0;
-    let mut compound: String = pieces[1].into();
-    // let mut compound_head = create_list(compound);
+    let mut pair_frequency: HashMap<(char, char), i64> = HashMap::new();
+    let chars: Vec<char> = compound.chars().collect();
+
+    // Determine the frequency of pairs
+    for i in 0..compound.len() - 1 {
+        let a = chars[i];
+        let b = chars[i+1];
+        let counter = pair_frequency.entry((a, b)).or_insert(0);
+        *counter += 1;
+    }
+
+    print_pair_frequency(&pair_frequency);
+
+    // Increment the compounds
+    for _ in 0..10 {
+        let mut new_pair_frequency: HashMap<(char, char), i64> = HashMap::new();
+
+        for (pair, count) in pair_frequency {
+            let char_to_insert = *rules.get(&pair).unwrap();
+            let left_pair = (pair.0, char_to_insert);
+            let left_counter = new_pair_frequency.entry(left_pair).or_insert(0);
+            *left_counter += count;
+
+            let right_pair = (char_to_insert, pair.1);
+            let right_counter = new_pair_frequency.entry(right_pair).or_insert(0);
+            *right_counter += count;
+        }
+
+        pair_frequency = new_pair_frequency;
+        print_pair_frequency(&pair_frequency);
+        //println!("pair_frequency = {:?}", pair_frequency);
+    }
+
+
+
+    println!("hi");
+
+    // let mut counter = 0;
+    // println!("{}\n", compound);
     //
-    // println!("aaa");
-    //
-    // while counter < 40 {
-    //     compound = run_step_v2(&rules, &mut compound_head);
+    // while counter < 10 {
+    //     compound = run_step(&rules, &compound);
+    //     println!("{}\n", compound);
     //     counter += 1;
-    //     // println!("counter = {}", counter);
-    //     counter += 1
     // }
 
-
-    panic!("aaa");
-    // for char in compound.chars() {
-    //     let counter = char_to_count.entry(char).or_insert(0);
-    //     *counter += 1;
-    // }
-    //
-    // let max = char_to_count.values().max().unwrap();
-    // let min = char_to_count.values().min().unwrap();
-    //
-    // return max - min;
+    return count_delta_v2(&pair_frequency);
 }
 
-// fn create_list(text: String) -> &'static mut Node {
-//     let chars: Vec<char> = text.chars().collect();
-//     let mut head = Node { value: chars[0], next: address::Nil };
-//
-//     for index in 1..chars.len() {
-//         head.append(chars[index])
-//     }
-//
-//     return &mut head;
-// }
+fn print_pair_frequency(freq: &HashMap<(char, char), i64>) {
+    for entry in freq {
+        print!("{}{} = {} ", entry.0.1, entry.0.1, entry.1)
+    }
+
+    print!("\n");
+}
+
+fn count_delta_v2(pair_frequency: &HashMap<(char, char), i64>) -> i64 {
+    let mut char_to_count: HashMap<char, i64> = HashMap::new();
+
+    for ((a, b), count) in pair_frequency {
+        let a_counter = char_to_count.entry(*a).or_insert(0);
+        *a_counter += count;
+
+        let b_counter = char_to_count.entry(*b).or_insert(0);
+        *b_counter += count;
+    }
+
+    let max = *char_to_count.values().max().unwrap();
+    let min = *char_to_count.values().min().unwrap();
+    return max - min;
+}
 
 #[cfg(test)]
 mod tests {
     use super::*;
     use crate::util::*;
     const FILENAME: &str = "./resources/2021/14.txt";
+    const EXAMPLE: &str = "NNCB
+
+CH -> B
+HH -> N
+CB -> H
+NH -> C
+HB -> C
+HC -> B
+HN -> C
+NN -> C
+BH -> H
+NC -> B
+NB -> B
+BN -> B
+BB -> N
+BC -> B
+CC -> N
+CN -> C";
 
     #[test]
     fn puzzle_part_a() {
@@ -223,45 +174,11 @@ mod tests {
 
     #[test]
     fn example_part_a() {
-        assert_eq!(part_a("NNCB
-
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C".into()), 1588);
+        assert_eq!(part_a(EXAMPLE.into()), 1588);
     }
 
     #[test]
     fn example_part_b() {
-        assert_eq!(part_b("NNCB
-
-CH -> B
-HH -> N
-CB -> H
-NH -> C
-HB -> C
-HC -> B
-HN -> C
-NN -> C
-BH -> H
-NC -> B
-NB -> B
-BN -> B
-BB -> N
-BC -> B
-CC -> N
-CN -> C".into()), 2188189693529);
+        assert_eq!(part_b(EXAMPLE.into()), 2188189693529);
     }
 }
