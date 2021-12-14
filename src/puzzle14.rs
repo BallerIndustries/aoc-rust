acb90f6b5b71155cd9771920aecd909e19dcf227
Angus Cheng <baller.industries@hotmail.com>
2021-12-15 01:38:07 +0800

    cleaning up, de fudging

--- src/puzzle14.rs
+++ src/puzzle14.rs
@@ -1,10 +1,9 @@
 use std::collections::{HashMap};
-use std::os::raw::c_double;
 
 pub fn simulate(text: String, iterations: i32) -> i64 {
     let pieces: Vec<&str> = text.split("\n\n").collect();
     let mut rules: HashMap<(char, char), char> = HashMap::new();
-    let mut compound: String = pieces[0].into();
+    let compound: String = pieces[0].into();
 
     for rule in pieces[1].lines() {
         let tmp: Vec<&str> = rule.split(" -> ").collect();
@@ -14,7 +13,14 @@ pub fn simulate(text: String, iterations: i32) -> i64 {
     }
 
     let mut pair_frequency: HashMap<(char, char), i64> = HashMap::new();
+    let mut char_frequency: HashMap<char, i64> = HashMap::new();
     let chars: Vec<char> = compound.chars().collect();
+
+    // Determine the frequency of chars
+    for char in &chars {
+        let counter = char_frequency.entry(*char).or_insert(0);
+        *counter += 1;
+    }
 
     // Determine the frequency of pairs
     for i in 0..compound.len() - 1 {
@@ -24,14 +30,16 @@ pub fn simulate(text: String, iterations: i32) -> i64 {
         *counter += 1;
     }
 
-    print_pair_frequency(&pair_frequency);
-
     // Increment the compounds
     for _ in 0..iterations {
         let mut new_pair_frequency: HashMap<(char, char), i64> = HashMap::new();
 
         for (pair, count) in pair_frequency {
             let char_to_insert = *rules.get(&pair).unwrap();
+
+            let char_counter = char_frequency.entry(char_to_insert).or_insert(0);
+            *char_counter += count;
+
             let left_pair = (pair.0, char_to_insert);
             let left_counter = new_pair_frequency.entry(left_pair).or_insert(0);
             *left_counter += count;
@@ -42,10 +50,9 @@ pub fn simulate(text: String, iterations: i32) -> i64 {
         }
 
         pair_frequency = new_pair_frequency;
-        print_pair_frequency(&pair_frequency);
     }
 
-    return count_delta_v2(&pair_frequency);
+    return count_delta_v2(&char_frequency);
 }
 
 
@@ -57,29 +64,10 @@ pub fn part_b(text: String) -> i64 {
     return simulate(text, 40);
 }
 
-fn print_pair_frequency(freq: &HashMap<(char, char), i64>) {
-    for ((a, b), count) in freq {
-        print!("{}{} = {} ", *a, *b, *count)
-    }
-
-    print!("\n");
-}
-
-fn count_delta_v2(pair_frequency: &HashMap<(char, char), i64>) -> i64 {
-    let mut char_to_count: HashMap<char, i64> = HashMap::new();
-
-    for ((a, b), count) in pair_frequency {
-        let a_counter = char_to_count.entry(*a).or_insert(0);
-        *a_counter += count;
-
-        let b_counter = char_to_count.entry(*b).or_insert(0);
-        *b_counter += count;
-    }
-
+fn count_delta_v2(char_to_count: &HashMap<char, i64>) -> i64 {
     let max = *char_to_count.values().max().unwrap();
     let min = *char_to_count.values().min().unwrap();
-    let delta = max as f64 - min as f64;
-    return (delta / 2f64).ceil() as i64;
+    return max - min;
 }
 
 #[cfg(test)]
@@ -115,7 +103,7 @@ CN -> C";
     #[test]
     fn puzzle_part_b() {
         let text = read_all_text(FILENAME);
-        assert_eq!(part_b(text), 0)
+        assert_eq!(part_b(text), 3152788426516)
     }
 
     #[test]
